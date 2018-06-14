57f4bcc6073497da0ed3570713ffe0744ed5d790
Grigor Yenoqyan <grigor.yenoqyan@gmail.com>
2018-06-14 19:43:47 +0400

    Edited News page

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -4,83 +4,34 @@ import Title from "../Title";
 import "./News.css";
 import PageContainer from '../PageContainer';
 import { news } from "../../data.js";
-import ArticleName from '../ArticleName';
 
-import Description from "../Description";
-
-
-const News = () => {
-    return (
-        <PageContainer>
-            <ColorContainer color='white'>
-                <Title color="black">News</Title>
-            </ColorContainer>
-            {
-                news.map((article, i) => {
-                    let cond = i % 2;
-                    let color = cond ? "black" : "white";
-                    let reverse = cond ? "news-content-wrapper-reverse" : "";
-                    let content = (
-                        <div>
-                            <div>
-                                <ArticleName color={color}>{article.title}</ArticleName>
-                            </div>
-                            <div className={`news-content-wrapper ${reverse}`}>
-                                <div className="img-wrapper">
-                                    <img 
-                                        src={article.image} 
-                                        alt="" 
-                                        className="article-image" 
-                                    />
-                                </div>
-                                <div className="desc-wrapper">
-                                    <Description  color={color}>{article.content}></Description>
-                                </div>
-                            </div>
-                        </div>
-                    );
-                    return (
-                        <div key={article.id}>
-                            {
-                                cond ? 
-                                    <ColorContainer color='white'>
-                                        {content}
-                                    </ColorContainer> : 
-                                    <ColorContainer color='black'>
-                                        {content}
-                                    </ColorContainer>
-                            }
-                            
-                        </div>
-                    )
-                })
-            }
-        </PageContainer>
-        
-    )
-}
+import NewsContent from "./NewsContent";
 
 
-
-
-
-
-
-
-
-
-// const News = () => {
-   
-
-// return (
-//     <ColorContainer>
-//         <ul className="newss">
-//             {arrays.map((el) =>( <li  key={el.news} className='news'> <p className="newsParagraphs">{el.news} </p>  </li>))}
-//         </ul>
-//         </ColorContainer>
-    
-     
-//    )
-// }
+const News = () => (
+    <PageContainer>
+        <ColorContainer color='white'>
+            <Title color="black">News</Title>
+        </ColorContainer>
+        {
+            news.map((article, i) => {
+                const content = <NewsContent article={article} index={i} />;
+                return (
+                    <div key={article.id}>
+                        {
+                            i % 2 ?
+                                <ColorContainer color='white'>
+                                    {content}
+                                </ColorContainer> :
+                                <ColorContainer color='black'>
+                                    {content}
+                                </ColorContainer>
+                        }
+                    </div>
+                )
+            })
+        }
+    </PageContainer>
+)
 
 export default News;
