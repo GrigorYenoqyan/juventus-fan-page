aa6d12506fe8cbf2edbd394a6fd4ec40c66738fb
nargit2106@gmail.com <nargit2106@gmail.com>
2018-06-15 22:27:20 +0400

    Added club block and other minor changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,13 @@ import Thoughts from '../Thoughts';
 import TrophySection from '../TrophySection';
 import PageContainer from '../PageContainer';
 import './Home.css';
+import Club from './Club';
 
 const Home = () => {
     return (
         <PageContainer>
             <Thoughts />
+            <Club />
             <TrophySection />
         </PageContainer>
     )
