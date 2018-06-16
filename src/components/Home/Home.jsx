e8de478a0c83de811fd35f89f38704a2545b0fb7
nargit2106@gmail.com <nargit2106@gmail.com>
2018-06-16 04:21:41 +0400

    path fix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,14 +4,15 @@ import TrophySection from '../TrophySection';
 import PageContainer from '../PageContainer';
 import './Home.css';
 import LastMatch from '../LastMatch';
+import Club from './Club';
 
 const Home = () => {
     return (
         <PageContainer>
             <Thoughts />
+            <LastMatch />
             <Club />
             <TrophySection />
-            <LastMatch />
         </PageContainer>
     )
 }
